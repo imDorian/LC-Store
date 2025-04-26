4bfbc9bada1434a62b4d46de3bc5e0b31388328d
imDorian <dorians.developer@gmail.com>
2025-04-26 21:47:16 +0200

    console

--- src/utils/getProducts.jsx
+++ src/utils/getProducts.jsx
@@ -2,7 +2,7 @@ import { API_URL } from "../constants/API_URL"
 import { TOKEN } from "../constants/TOKEN"
 
 export async function getProducts() {
-    console.log(API_URL, TOKEN)
+
     const response = await window.fetch(`${API_URL}products?populate=media`, {
         method: 'GET',
         headers: {
